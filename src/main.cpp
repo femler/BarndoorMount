673ba41f7e75d3f5711320ee7603526e2ac8f7ba
femler <femler+git@gmail.com>
2020-11-23 22:59:26 +0100

    try and error

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,14 @@
 
 #define interval 5000
 
-bool Toggle=LOW;
-unsigned long previousMillis = 0;
+//bool Toggle=LOW;
+unsigned long prevMill = 0;
+
+boolean isTracking=false;
+boolean isRewinding=false;
+
+int goTracking = 1;
+int goRewind = 1;
 
 // 2-wire basic config, microstepping is hardwired on the driver
 AccelStepper AccStep = AccelStepper(1,STEP,DIR);
@@ -42,35 +48,55 @@ void RunFwd(){
   Serial.println("Jetzt gehts vorwärts");
   delay(500);
   AccStep.setSpeed(100);
-  
 }
 
 void RunRew(){
   Serial.println("Jetzt gehts rückwärts");
   delay(500);
-  AccStep.setSpeed(-100);
+  AccStep.setSpeed(-500);
 }
 
 void setup() {
   AccStep.setMaxSpeed(2000);
   //AccStep.setAcceleration(20);
+  pinMode(TrackBtn,INPUT_PULLUP);
+  pinMode(RewBtn,INPUT_PULLUP);
+
   Serial.begin(9600);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
   
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    if (Toggle) {
-      Toggle = !Toggle;
-      RunFwd();
-    } else {
-      Toggle = !Toggle;
-      RunRew();
-    }
+  
+  // if (currentMillis - previousMillis >= interval) {
+  //   previousMillis = currentMillis;
+  //   if (Toggle) {
+  //     Toggle = !Toggle;
+  //     RunFwd();
+  //   } else {
+  //     Toggle = !Toggle;
+  //     RunRew();
+  //   }
 
+  // }
+
+  goTracking = digitalRead(TrackBtn);
+  goRewind = digitalRead(RewBtn);
+
+  if (goTracking==LOW){
+    isTracking = !isTracking;
+    RunFwd();
   }
-  AccStep.runSpeed();
+
+  if (goRewind==LOW){
+    isRewinding = !isRewinding;
+    isTracking = false;
+    RunRew();
+    AccStep.runSpeed();
+  }
+
+  if (isTracking) {
+    AccStep.runSpeed();
+  }
 }
 
